67453648e65b4f3ddd125585209e4e19c5a23bc0
Martín <magadiflo@gmail.com>
2023-11-13 23:48:49 -0500

    Validaciones Básicas

--- src/app/forms/pages/basic-page/basic-page.component.ts
+++ src/app/forms/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'basic-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,],
   templateUrl: './basic-page.component.html',
   styles: ``
 })
 export class BasicPageComponent {
+
+  private _formBuilder = inject(FormBuilder);
+
+  public formGroup: FormGroup = this._formBuilder.group({
+    doYouPayAttentionToClasses: [false],
+    doYouSubmitYourAssignmentsOnTime: [false],
+    missingClasses: [false],
+    observations: ['', Validators.required],
+    dataFather: this._formBuilder.group({
+      names: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+    }),
+    dataMother: this._formBuilder.group({
+      names: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+    }),
+  });
+
+  public saveData(): void {
+    console.log(this.formGroup.value);
+    this.formGroup.markAllAsTouched();
+  }
+
+  public get observationsField() {
+    return this.formGroup.controls['observations'];
+  }
+
+  public get dataFatherField() {
+    return this.formGroup.controls['dataFather'] as FormGroup;
+  }
+
+  public get dataMotherField() {
+    return this.formGroup.controls['dataMother'] as FormGroup;
+  }
 
 }
