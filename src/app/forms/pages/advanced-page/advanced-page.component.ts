1c08355b74eedcc2852653dc39a453b61476f556
Martín <magadiflo@gmail.com>
2023-11-16 23:40:29 -0500

    Implementando el formulario en advanced page

--- src/app/forms/pages/advanced-page/advanced-page.component.ts
+++ src/app/forms/pages/advanced-page/advanced-page.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'advanced-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './advanced-page.component.html',
   styles: ``
 })
 export class AdvancedPageComponent {
+
+  private _formBuilder = inject(FormBuilder);
+
+  public formGroup: FormGroup = this._formBuilder.group({
+    doYouPayAttentionToClasses: [false],
+    doYouSubmitYourAssignmentsOnTime: [false],
+    missingClasses: [false],
+    observations: ['', Validators.required],
+    dataFather: this._formBuilder.group({
+      names: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+    }),
+    dataMother: this._formBuilder.group({
+      names: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+    }),
+  });
+
+  public saveData(): void {
+    console.log(this.formGroup.value);
+    this.formGroup.markAllAsTouched();
+  }
 
 }
